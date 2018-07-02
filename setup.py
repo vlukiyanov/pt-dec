6c0008e7f2e45cb3b7dc8683ce063583ff07638c
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2018-07-03 00:04:59 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+setup(
+    name='pt-dec',
+    version='1.0',
+    description='PyTorch implementation of DEC.',
+    author='Vladimir Lukiyanov',
+    author_email='vladimir.lukiyanov@me.com',
+    url='https://github.com/vlukiyanov/pt-dec/',
+    download_url='',
+    license='MIT',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    install_requires=[
+        'numpy>=1.13.3',
+        'torch>=0.4.0',
+        'scipy>=1.0.0',
+        'pandas>=0.21.0',
+        'visdom>=0.1.05',
+        'click>=6.7',
+        'xlrd>=1.0.0',
+        'cytoolz>=0.9.0.1',
+        'tqdm>=4.11.2',
+        'scikit-learn>=0.19.1'
+    ],
+    packages=['pt-dec']
+)

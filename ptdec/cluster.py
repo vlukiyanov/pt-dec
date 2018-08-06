aae39d66a925e581b73c4b79e2be227ecd101c69
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2018-08-07 00:08:27 +0100

    fix a small typo in the function about special case handling

--- ptdec/cluster.py
+++ ptdec/cluster.py
@@ -46,7 +46,6 @@ class ClusterAssignment(nn.Module):
         """
         norm_squared = torch.sum((batch.unsqueeze(1) - self.cluster_centers)**2, 2)
         numerator = 1.0/(1.0 + (norm_squared/self.alpha))
-        if self.alpha != 1.0:
-            power = -float(self.alpha + 1) / 2
-            numerator = numerator**power
+        power = -float(self.alpha + 1) / 2
+        numerator = numerator**power
         return (numerator.t()/torch.sum(numerator, 1)).t()

b7b69db8f0162adfe4a7e2fa24189e7bdaaf2679
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2018-12-23 16:18:12 +0000

    move tests

---
+++ tests/test_cluster.py
@@ -0,0 +1,35 @@
+import torch
+from torch.autograd import Variable
+from unittest import TestCase
+
+from ptdec.cluster import ClusterAssignment
+
+
+class TestClusterAssignment(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.ca = ClusterAssignment(
+            2,
+            2,
+            cluster_centers=torch.Tensor([
+                [-1, -1],
+                [1, 1]
+            ]).float()
+        )
+
+    def test_forward(self):
+        """
+        Basic test to check that the calculation is equivalent to the one in the paper.
+        """
+        test_tensor = Variable(torch.Tensor([-2, -2]).float().unsqueeze(0))
+        den = float(1) / 3 + float(1) / 19
+        gold = torch.Tensor([(float(1) / 19) / den, (float(1) / 3) / den])
+        output = self.ca(test_tensor).data
+        self.assertAlmostEqual(
+            (gold - output).numpy()[0][0],
+            0.0
+        )
+        self.assertAlmostEqual(
+            (gold - output).numpy()[0][1],
+            0.0
+        )

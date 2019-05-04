66ae0938b4eeb84e01241448761799b0c98bc3ca
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2019-05-04 21:54:43 +0100

    Fix the cluster assignment function (#10)

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -1,5 +1,4 @@
 import torch
-from torch.autograd import Variable
 from unittest import TestCase
 
 from ptdec.cluster import ClusterAssignment
@@ -9,8 +8,8 @@ class TestClusterAssignment(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.ca = ClusterAssignment(
-            2,
-            2,
+            cluster_number=2,
+            embedding_dimension=2,
             cluster_centers=torch.Tensor([
                 [-1, -1],
                 [1, 1]
@@ -21,9 +20,9 @@ class TestClusterAssignment(TestCase):
         """
         Basic test to check that the calculation is equivalent to the one in the paper.
         """
-        test_tensor = Variable(torch.Tensor([-2, -2]).float().unsqueeze(0))
+        test_tensor = torch.Tensor([-2, -2]).float().unsqueeze(0)
         den = float(1) / 3 + float(1) / 19
-        gold = torch.Tensor([(float(1) / 19) / den, (float(1) / 3) / den])
+        gold = torch.Tensor([(float(1) / 3) / den, (float(1) / 19) / den])
         output = self.ca(test_tensor).data
         self.assertAlmostEqual(
             (gold - output).numpy()[0][0],
